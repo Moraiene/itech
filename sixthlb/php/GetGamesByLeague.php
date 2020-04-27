7115fd25a04b0592aba0e11c95cba42ee0f771b4
Vladislav Ponamaryov <38539528+Moraiene@users.noreply.github.com>
2020-04-27 14:11:43 +0300

    added sixthlb
    added sixthlb

---
+++ sixthlb/php/GetGamesByLeague.php
@@ -0,0 +1,16 @@
+
+<?php
+ require_once __DIR__ . "/vendor/autoload.php";
+ $collection = (new MongoDB\Client) -> dbforlab -> games;
+
+$league = $_GET["league"];
+$cond = array("league" => $league);
+    $cursor = $collection -> find($cond);
+    $res = [];
+
+    foreach($cursor as $document){
+        array_push($res, $document);
+        
+    }
+    echo json_encode($res);
+?>
